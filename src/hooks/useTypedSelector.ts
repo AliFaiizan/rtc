71d694438a1c1d860ba504ed18dc032b5924a958
alifaiizan <alifaizanahmad@gmail.com>
2023-03-24 23:11:03 +0100

    docs

--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,5 +1,5 @@
 import { useSelector,TypedUseSelectorHook } from "react-redux";
 
 import { RootState } from "../states/reducers";
-
+//this is used for redux typing while returning the state
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
