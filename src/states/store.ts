f164fbb9dbd04636fefdbfda2a1fd56133b16ef4
alifaiizan <alifaizanahmad@gmail.com>
2023-04-12 23:23:22 +0200

    thunk

--- src/states/store.ts
+++ src/states/store.ts
@@ -4,4 +4,5 @@ import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
+//redux thunk is a middleware that allows you to return functions, rather than just actions, within Redux action creators. This allows for delayed actions, including working with promises.
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
